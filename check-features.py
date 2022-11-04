73a92e7401a7cf46dc014783ae44720948a1a94b
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2022-11-04 16:08:29 +0100

    move cpu features to yaml as well
    Makes it easier to handle adding new ones.

--- check-features.py
+++ check-features.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 import tables
+import yaml
 
 for soc in tables.socs:
     for feature in soc["features"]:
         if feature not in tables.cpu_features:
-            print(
-                f"""
-"{feature}": {{
-    "archv": 8.0,
-}}, """
-            )
+            tables.cpu_features[feature] ={"archv": 8.0}
+
+with open("cpu_features.yml", "w") as yml:
+    yaml.dump(tables.cpu_features, yml)
