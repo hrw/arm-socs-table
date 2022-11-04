243d676fd20938259a83b7d6d78eac39b4e7fe68
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2022-11-04 16:14:27 +0100

    added missing whitespace

--- check-features.py
+++ check-features.py
@@ -6,7 +6,7 @@ import yaml
 for soc in tables.socs:
     for feature in soc["features"]:
         if feature not in tables.cpu_features:
-            tables.cpu_features[feature] ={"archv": 8.0}
+            tables.cpu_features[feature] = {"archv": 8.0}
 
 with open("cpu_features.yml", "w") as yml:
     yaml.dump(tables.cpu_features, yml)
