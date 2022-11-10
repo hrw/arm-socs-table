688c69f650c763099e49ca0353406145e6a3fe29
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2022-11-10 11:44:10 +0100

    new way of storing cpu core data
    As SoC can have cpu cores from more than one vendor (example: Snapdragon
    865 with Qualcomm and Arm cores) that data needs to be stored other way.
    
    All core data will end in 'cores' part.
    
    'convert-part-to-cores.py' script does the conversion

---
+++ convert-part-to-cores.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import tables
+import yaml
+
+def hexint_presenter(dumper, data):
+    return dumper.represent_int(hex(data))
+
+
+yaml.add_representer(int, hexint_presenter)
+
+for soc in tables.socs:
+    cores = []
+    for part in soc['part']:
+        try:
+            if soc['revision']:
+                pass
+        except KeyError:
+            soc['revision'] = 0
+        try:
+            if soc['variant']:
+                pass
+        except KeyError:
+            soc['variant'] = 0
+        cores.append({
+            'part': part,
+            'implementer': soc['implementer'],
+            'revision': soc['revision'],
+            'variant': soc['variant'],
+        })
+    soc['cores'] = cores
+    del soc['implementer']
+    del soc['part']
+    del soc['variant']
+    del soc['revision']
+
+with open("socs.yml", "w") as yml:
+    yaml.dump(tables.socs, yml)
